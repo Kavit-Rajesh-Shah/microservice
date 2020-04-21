a0c340314fe80f607d3aa22822b3dc209c490adb
Kavit <kavitsh@gmail.com>
2020-04-21 14:40:24 +0530

    Removed unused packages

--- src/main/java/com/microservice/MicroserviceApplication.java
+++ src/main/java/com/microservice/MicroserviceApplication.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.classic.Level;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
