1dfc1d69dc209f905cdbcc5fb634fdab6f6ad4cc
Kavit <kavitsh@gmail.com>
2020-04-21 11:46:24 +0530

    Warning Logging

--- src/main/java/com/microservice/MicroserviceApplication.java
+++ src/main/java/com/microservice/MicroserviceApplication.java
@@ -9,17 +9,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
 @RestController
 @RequestMapping("/helloworld")
 @SpringBootApplication
 public class MicroserviceApplication {
 
+	int strLength = 0;
+	Logger logger = LoggerFactory.getLogger(MicroserviceApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
 	
 	@GetMapping("/{name}")
 	public String greet(@PathVariable String name) {
+		
+		strLength = name.length();
+		if(strLength = 5) 
+			logger.warn("User: " + name + " trying to use the greet service");
 		
 		return (LocalDateTime.now().getHour() < 12 ? "Good Morning " : "Have a nice day ahead ") 
 				+ name;
